89f02608c18fd7333e7dfa181b894cce2c56f552
Ondrej Macoszek <omacoszek1@gmail.com>
2023-02-28 14:12:12 +0100

    add demo frontend of food ordering app without order feature

---
+++ food-ordering-app/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,45 @@
+import { useContext, useEffect, useState } from "react";
+
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
+
+const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((currNum, item) => {
+    return currNum + item.amount;
+  }, 0);
+
+  const buttonClasses = `${classes.button} ${
+    buttonIsHighlighted ? classes.bump : ""
+  } `;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={buttonClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
